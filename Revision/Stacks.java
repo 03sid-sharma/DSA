3b184fc37a7ddd203c76951d8aa3ee7344dd1538
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2023-08-04 12:52:47 +0530

    feat : balanced expr

--- Revision/Stacks.java
+++ Revision/Stacks.java
@@ -3,7 +3,9 @@ import java.util.Stack;
 class Main{
     public static void main(String[] args) {
         StringReverser str = new StringReverser();
-        System.out.println(str.reverse(null));
+        // System.out.println(str.reverse(null));
+
+        System.out.println(str.balancedExpr("{p[0-<0->]}"));
     }
 }
 
@@ -22,5 +24,26 @@ class StringReverser{
             reversed.append(stack.pop());
 
         return reversed.toString();
+    }
+
+    public boolean balancedExpr(String str){
+        Stack<Character> stack = new Stack<>();
+        for (Character character : str.toCharArray()) {
+            if(character == '<' || character == '{' || character == '(' || character == '[')
+                stack.push(character);
+            
+            if(character == '>' || character == '}' || character == ')' || character == ']'){
+                if(stack.empty())
+                    return false;
+
+                var top = stack.pop();
+                if((character == '>' && top != '<') ||
+                    (character == '}' && top != '{') ||
+                    (character == ')' && top != '(') ||
+                    (character == ']' && top != '['))
+                    return false;
+            }
+        }
+        return true;
     }
 }
