43a08916e1a1f0e146dbee4135511a5ec355a86f
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2023-08-08 11:22:33 +0530

    feat : stack using array

--- Revision/Stacks.java
+++ Revision/Stacks.java
@@ -1,12 +1,54 @@
-import java.util.Stack;
+import java.util.*;
 
 class Main{
     public static void main(String[] args) {
-        StringReverser str = new StringReverser();
+        // StringReverser str = new StringReverser();
         // System.out.println(str.reverse(null));
+        // System.out.println(str.balancedExpr("{p[0-<0->]}"));
+        Stck stck = new Stck();
+        stck.push(10);
+        stck.push(20);
+        stck.push(30);
+        stck.push(40);
+        stck.pop();
+        System.out.println(stck.isEmpty());
 
-        System.out.println(str.balancedExpr("{p[0-<0->]}"));
     }
+}
+
+class Stck{
+    private int arr[] = new int[5];
+    private int count = 0;
+
+    public void push(int item){
+        if(count == arr.length)
+            throw new StackOverflowError();
+        arr[count++] = item;
+    }
+
+    public int peek(){
+        if(count == 0)  
+            throw new IllegalStateException();
+        return arr[count - 1];
+    }
+
+    public boolean isEmpty(){
+        return count ==  0;
+    }
+
+    public int pop(){
+        if(count == 0) 
+            throw new IllegalStateException();
+        return arr[--count];
+    }
+
+    @Override
+    public String toString(){
+        var items = Arrays.copyOfRange(arr, 0 , count);
+        return Arrays.toString(items);
+    } 
+    
+
 }
 
 class StringReverser{
