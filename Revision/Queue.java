8f7a1d75035bff43119189a1bc833e1b8587dca7
Siddharth Sharma <65253410+03sid-sharma@users.noreply.github.com>
2023-09-14 20:20:52 +0530

    feat : queue with linked list

--- Revision/Queue.java
+++ Revision/Queue.java
@@ -9,14 +9,14 @@ import java.util.Stack;
 class Main {
 
     public static void main(String[] args) {
-        Queue<Integer> queue = new ArrayDeque<>();
-        queue.add(10);
-        queue.add(20);
-        queue.add(30);
-        queue.add(40);
-        queue.add(50);
-        reverseFirstKElements(queue, 3);
-        System.out.println(queue);
+        // Queue<Integer> queue = new ArrayDeque<>();
+        // queue.add(10);
+        // queue.add(20);
+        // queue.add(30);
+        // queue.add(40);
+        // queue.add(50);
+        // reverseFirstKElements(queue, 3);
+        // System.out.println(queue);
         // reverse(queue);
 
         // ArrrayQueue queue = new ArrrayQueue(5);
@@ -45,16 +45,25 @@ class Main {
         // while (!pq.isEmpty()) {
         //     System.out.println(pq.remove());
         // }
+
+        // LinkedListQueue llq = new LinkedListQueue();
+        // llq.enqueue(10);
+        // llq.enqueue(20);
+        // llq.enqueue(30);
+        // System.out.println(llq.size());
+        // llq.dequeue();
+        // System.out.println(llq.peek());
+        // llq.print();
     }
 
-    // public static void reverse(Queue<Integer> queue){
-    //     Stack<Integer> stack = new Stack<>();
-    //     while (!queue.isEmpty()) 
-    //         stack.push(queue.remove());
+    public static void reverse(Queue<Integer> queue){
+        Stack<Integer> stack = new Stack<>();
+        while (!queue.isEmpty()) 
+            stack.push(queue.remove());
         
-    //     while (!stack.isEmpty()) 
-    //         queue.add(stack.pop());
-    // }
+        while (!stack.isEmpty()) 
+            queue.add(stack.pop());
+    }
 
     public static void reverseFirstKElements(Queue<Integer> queue, int k){
         Stack<Integer> stack = new Stack<>();
@@ -68,6 +77,7 @@ class Main {
         for (int index = 0; index < queue.size() - k ; index++)
             queue.add(queue.remove());
     }
+
 }
 /**
  * Queue
@@ -182,5 +192,72 @@ class PriorityQueue{
 
     public boolean isEmpty() {
         return count == 0;
+    }
+}
+
+class LinkedListQueue{
+    private class Node{
+        private int value;
+        private Node next;
+
+        public Node(int value){
+            this.value = value;
+        }
+    }
+
+    public Node first;
+    public Node last;
+    public int count;
+
+    public void enqueue(int item){
+        Node node = new Node(item);
+        if(isEmpty())  
+            first = last = node;
+        else{
+            last.next = node;
+            last = node;
+        }
+        count++;        
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    public int dequeue(){
+        int value;
+        if(isEmpty())
+            throw new IllegalStateException();
+        
+        if(first == last){
+            value = first.value;
+            first = last = null;
+        }else{
+            value = first.value;
+            var second = first.next;
+            first.next = null;
+            first = second;
+        }
+        count--;
+        return value;
+    }
+
+    public int peek(){
+        if(isEmpty())
+            throw new IllegalStateException();
+
+        return first.value;
+    }
+
+    public int size(){
+        return count;
+    }
+
+    public void print(){
+        var current = first;
+        while (current != null) {
+            System.out.println(current.value);
+            current = current.next;
+        }
     }
 }
