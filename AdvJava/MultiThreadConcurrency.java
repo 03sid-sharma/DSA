d1bf03250fabb78b0d21daae626cd3105fd04e83
Siddharth Sharma <03sid.sharma@gmail.com>
2024-08-22 12:10:16 +0530

    feat: threads init

---
+++ AdvJava/MultiThreadConcurrency.java
@@ -0,0 +1,8 @@
+package AdvJava;
+
+public class MultiThreadConcurrency {
+    public static void main(String[] args) {
+        System.out.println(Thread.activeCount());
+        System.out.println(Runtime.getRuntime().availableProcessors());
+    }
+}
